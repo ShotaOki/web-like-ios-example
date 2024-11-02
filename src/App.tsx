b405d29b16a74bb64ff35b0ee570075f17145843
shota oki <oki.08031367557@i.softbank.jp>
2024-11-02 17:15:18 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,34 @@
 import { useState } from "react";
 import { useIOSNativeUI } from "./ios-native-ui/iOSNativeUI";
-import ActionModal from "./ios-native-ui/ActionModal";
+import ActionModalNavigation from "./ios-native-ui/ActionModalNavigation";
 
 function App() {
-  const { Button, AlertDialog, AlertDialogButton, Navigator, Page } =
-    useIOSNativeUI();
+  const { Button, AlertDialog, AlertDialogButton } = useIOSNativeUI();
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <Navigator
-        animation="lift"
-        renderPage={(route, navigator) => {
-          const { page } = route;
-          return (
-            <>
-              {page == "Main" && (
-                <Page>
-                  <Button onClick={() => setIsOpen(true)}>Show Dialog</Button>
-                  {/* Alert Dialog */}
-                  <AlertDialog isOpen={isOpen}>
-                    <div className="alert-dialog-title">Warning!</div>
-                    <div className="alert-dialog-content">
-                      An error has occurred!
-                    </div>
-                    <div className="alert-dialog-footer">
-                      <AlertDialogButton onClick={() => setIsOpen(false)}>
-                        Cancel
-                      </AlertDialogButton>
-                      <AlertDialogButton onClick={() => setIsOpen(false)}>
-                        OK
-                      </AlertDialogButton>
-                    </div>
-                  </AlertDialog>
-                  <Button
-                    onClick={() => navigator?.pushPage({ page: "ActionModal" })}
-                  >
-                    Show Action Modal
-                  </Button>
-                </Page>
-              )}
-              {page == "ActionModal" && (
-                <Page>
-                  <ActionModal></ActionModal>
-                </Page>
-              )}
-            </>
-          );
-        }}
-        initialRoute={{
-          page: "Main",
-        }}
-      />
-    </>
+    <ActionModalNavigation>
+      {(navigator) => (
+        <>
+          <Button onClick={() => setIsOpen(true)}>Show Dialog</Button>
+          {/* Alert Dialog */}
+          <AlertDialog isOpen={isOpen}>
+            <div className="alert-dialog-title">Warning!</div>
+            <div className="alert-dialog-content">An error has occurred!</div>
+            <div className="alert-dialog-footer">
+              <AlertDialogButton onClick={() => setIsOpen(false)}>
+                Cancel
+              </AlertDialogButton>
+              <AlertDialogButton onClick={() => setIsOpen(false)}>
+                OK
+              </AlertDialogButton>
+            </div>
+          </AlertDialog>
+          <Button onClick={() => navigator.showModal()}>
+            Show Action Modal
+          </Button>
+        </>
+      )}
+    </ActionModalNavigation>
   );
 }
 
